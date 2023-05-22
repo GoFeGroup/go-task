9567ea29db1d5a317e69d2714a09c58551a933be
anhongkui <anhongkui@jd.com>
2023-05-22 17:40:23 +0800

    change time.Time to int64

--- task.go
+++ task.go
@@ -11,23 +11,23 @@ type Task struct {
 	e          *list.Element
 	fn         func(*Context) // 任务的回调函数
 	interval   int            // 间隔
-	createTime time.Time      // 任务创建时间
-	nextTime   time.Time      // 任务启动时间
+	createTime int64          // 任务创建时间
+	nextTime   int64          // 任务启动时间
 	ctx        *Context
 }
 
 // Can 任务是否达到了启动的条件
 func (t *Task) Can() bool {
-	return time.Now().After(t.nextTime)
+	return time.Now().Unix() > t.nextTime
 }
 
 func (t *Task) Run() {
 	t.fn(t.ctx)
-	t.nextTime = time.Now().Add(time.Second * time.Duration(t.interval))
+	t.nextTime = time.Now().Unix() + int64(t.interval)
 }
 
 func (t *Task) NextTick() int64 {
-	return int64(t.nextTime.Sub(time.Now()) / time.Second)
+	return t.nextTime - time.Now().Unix()
 }
 
 func NewTask(key string, fn func(*Context), interval int) *Task {
@@ -39,8 +39,8 @@ func NewTaskWithContext(key string, fn func(*Context), interval int, ctx *Contex
 		key:        key,
 		fn:         fn,
 		interval:   interval,
-		createTime: time.Now(),
-		nextTime:   time.Now().Add(time.Second * time.Duration(interval)),
+		createTime: time.Now().Unix(),
+		nextTime:   time.Now().Unix() + int64(interval),
 		ctx:        ctx,
 	}
 	return t
